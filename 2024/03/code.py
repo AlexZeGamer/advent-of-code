9e5af90330fbd466f5b7f32db1fb14f2cd688f14
Alexandre Malfreyt <alexandre.malfreyt@gmail.com>
2024-12-03 07:12:54 +0100

    Day 3 Part 2 (WIP)

--- 2024/03/code.py
+++ 2024/03/code.py
@@ -12,8 +12,8 @@ with open('input.txt', 'r') as f:
 # Part 1
 total = 0
 
-regex = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')
-for match in regex.finditer(text):
+mul_regex = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')
+for match in mul_regex.finditer(text):
     a, b = map(int, match.groups())
     total += a * b
 
@@ -23,6 +23,19 @@ print(f'Part 1 : {total}')
 # Part 2
 total = 0
 
+text = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
+do_regex = re.compile(r'do\(\)')
+dont_regex = re.compile(r"don't\(\)")
+
+do_matches = [match.start() for match in do_regex.finditer(text)] # [(pos1), (pos2), ...]
+dont_matches = [match.start() for match in dont_regex.finditer(text)] # [(pos1), (pos2), ...]
+mul_matches = [(match.start(), *map(int, match.groups())) for match in mul_regex.finditer(text)] # [(pos1, a1, b1), (pos2, a2, b2), ...]
+
+for mul_match in mul_matches:
+    if any(do_match < mul_match[0] < dont_match for do_match in do_matches for dont_match in dont_matches):
+        continue
+    a, b = mul_match[1:]
+    total += a * b
 
 print(f'Part 2 : {total}')
