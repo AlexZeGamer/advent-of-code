c0112d64a9fdedcd9bdeee275ca64998fc007550
Alexandre MALFREYT <alexandre.malfreyt@synchrotron-soleil.fr>
2025-12-02 11:10:54 +0100

    [2025] Day 2 Part 1

---
+++ 2025/02/code.py
@@ -0,0 +1,37 @@
+# Advent of Code 2025 - Day 2
+# https://adventofcode.com/2025/day/2
+# Author: Alexandre MALFREYT
+
+# Parse input
+with open('input.txt', 'r') as f:
+    ranges = [tuple(map(int, line.split('-'))) for line in f.read().strip().split(',')]
+
+# Functions
+def find_invalid_ids(ranges):
+    total = 0
+    for r_start, r_end in ranges:
+        for n in range(r_start, r_end + 1):
+            n_str = str(n)
+            if not len(n_str)%2 and n_str[:len(n_str)//2] == n_str[len(n_str)//2:]:
+                total += n
+    return total
+
+
+# Part 1 & 2
+def part1(ranges):
+    return find_invalid_ids(ranges)
+
+def part2(ranges):
+    ...
+
+
+# Tests
+ranges_example = [(11,22), (95,115), (998,1012), (1188511880,1188511890), (222220,222224), (1698522,1698528), (446443,446449), (38593856,38593862), (565653,565659), (824824821,824824827), (2121212118,2121212124)]
+
+assert part1(ranges_example) == 1227775554
+# assert part2(ranges_example) == ...
+
+
+# Results
+print(f'Part 1 : {part1(ranges)}')
+# print(f'Part 2 : {part2(ranges)}')
