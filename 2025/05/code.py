2865d0691063020a5f21d4639130e8d15b622e11
Alexandre MALFREYT <alexandre.malfreyt@synchrotron-soleil.fr>
2025-12-05 09:30:07 +0100

    [2025] Day 5 Part 2

--- 2025/05/code.py
+++ 2025/05/code.py
@@ -3,7 +3,9 @@
 # Author: Alexandre MALFREYT
 
 # Comment (Part 1): This one seems too easy... I'm afraid of part 2.
-# Comment (Part 2): ...
+# Comment (Part 2): Ok that was a bit tricky but fun to solve (naive solution clearly wouldn't
+#                   work here, ranges are so big it gets stuck on the very first range and runs
+#                   out of memory)
 
 # Parse input
 with open('input.txt', 'r') as f:
@@ -22,29 +24,34 @@ with open('input.txt', 'r') as f:
     # print("\nIngredients:")
     # for i in range(len(ingredients)):
     #     print(f'  Ingredient {i+1}: {ingredients[i]}')
-        
-
-
-# Functions
-...
 
 
 # Part 1 & 2
 def part1(fresh_ranges, ingredients):
     return len([ing for ing in ingredients if any(ing in r for r in fresh_ranges)])
 
-def part2(fresh_ranges, ingredients):
-    return ...
-
+def part2(fresh_ranges):
+    nb_fresh_ids = 0
+    for i, r in enumerate(fresh_ranges):
+        # check if this range overlaps with any other already added range (min or max in any of previous ranges), and cut it if so
+        for prev_r in fresh_ranges[:i]:
+            if r.start in prev_r:
+                r = range(prev_r.stop, r.stop + 1)
+            if r.stop in prev_r:
+                r = range(r.start, prev_r.start)
+        if r.start < r.stop:
+            nb_fresh_ids += len(r)
+    return nb_fresh_ids
+        
 
 # Tests
 fresh_ranges_example = [range(3, 5+1), range(10, 14+1), range(16, 20+1), range(12, 18+1)]
 ingredients_example = [1,5,8,11,17,32]
 
 assert part1(fresh_ranges_example, ingredients_example) == 3
-assert part2(fresh_ranges_example, ingredients_example) == ...
+assert part2(fresh_ranges_example) == 14
 
 
 # Results
 print(f'Part 1 : {part1(fresh_ranges, ingredients)}')
-print(f'Part 2 : {part2(fresh_ranges, ingredients)}')
+print(f'Part 2 : {part2(fresh_ranges)}')
