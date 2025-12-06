979a884443e08ca6ea115f9c118448ae23908083
Alexandre MALFREYT (Termux) <alexandre.malfreyt@gmail.com>
2025-12-06 13:26:15 +0100

    [2025] Day 6 Part 2

--- 2025/06/code.py
+++ 2025/06/code.py
@@ -5,24 +5,48 @@
 # Comment (part 1): Today I forgot my laptop so I'm forced to do it on my phone...
 #                   Fortunately today's challenge seems easy? at least part 1
 
+# Comment (part 2): Completely different parsing, but same logic.
+#                   Wasn't too complicated, just a bit tricky to write on a smartphone
+
 # Parse input
 with open('input.txt', 'r') as f:
-    lines = f.readlines()
-    numbers = [[int(x) for x in y.strip().split()] for y in lines[:-1]]
-    numbers = list(map(list, zip(*numbers))) # transpose numbers
+    lines = [l.strip("\n") for l in f.readlines()]
+
+def parse_part1(lines):
+    op_numbers = [[int(x) for x in y.strip().split()] for y in lines[:-1]]
+    op_numbers = list(map(list, zip(*op_numbers))) # transpose op_numbers
     operators = lines[-1].strip().split()
+    return op_numbers, operators
+
+def parse_part2(lines):
+    # parse from right to left to construct op_numbers_str
+    nb_ops = len(lines[0])
+    nb_numbers_per_op = len(lines)-1
+    op_numbers_str = []
+    
+    def _check_separator(lines, i):
+        return all([line[i]==" " for line in lines])
+
+    op_numbers_str.append([])
+    for i in range(nb_ops-1, -1, -1):
+        if _check_separator(lines, i):
+            op_numbers_str.append([])
+        else:
+            op_numbers_str[-1].append("")
+            for j in range(nb_numbers_per_op):
+                op_numbers_str[-1][-1] += lines[j][i]
+    op_numbers = [[int(n.strip()) for n in op] for op in op_numbers_str]
+    op_numbers = op_numbers[::-1] # reverse list for operations to be left to right
+    operators = lines[-1].strip().split()
+    return op_numbers, operators
 
 
 # Functions
-...
-
-
-# Part 1 & 2
-def part1(numbers, operators):
+def calc_operations(op_numbers, operators):
     total = 0
     for i in range(len(operators)):
         subtotal = 0 if operators[i] == "+" else 1
-        for number in numbers[i]:
+        for number in op_numbers[i]:
             if operators[i] == "+":
                 subtotal += number
             else: # "*"
@@ -30,18 +54,27 @@ def part1(numbers, operators):
         total += subtotal
     return total
 
-def part2(numbers, operators):
-    return ...
+
+# Part 1 & 2
+def part1(op_numbers, operators):
+    return calc_operations(op_numbers, operators)
+
+def part2(op_numbers, operators):
+    return calc_operations(op_numbers, operators)
 
 
 # Tests
-numbers_example = [[123, 45, 6], [328, 64, 98], [51, 387, 215], [64, 23, 314]]
-operators_example = ["*", "+", "*", "+"]
+example_lines = """\
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+""".split("\n")[:-1]
 
-assert part1(numbers_example, operators_example) == 4277556
-assert part2(numbers_example, operators_example) == ...
+assert part1(*parse_part1(example_lines)) == 4277556
+assert part2(*parse_part2(example_lines)) == 3263827
 
 
 # Results
-print(f'Part 1 : {part1(numbers, operators)}')
-print(f'Part 2 : {part2(numbers, operators)}')
+print(f'Part 1 : {part1(*parse_part1(lines))}')
+print(f'Part 2 : {part2(*parse_part2(lines))}')
